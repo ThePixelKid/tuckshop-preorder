ca9fff3be7c3548eabac1b35bfba5bd07e1fcbac
Richard Fayose <98465669+ThePixelKid@users.noreply.github.com>
2026-02-01 22:07:17 +0100

    Refactor Firebase initialization and config
    Updated Firebase SDK imports and configuration.

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+  apiKey: "AIzaSyDBmv2t6tB_LwdQLMXmtQQPjaKDjOE8w_U",
+  authDomain: "tuckshop-preorder.firebaseapp.com",
+  databaseURL: "https://tuckshop-preorder-default-rtdb.firebaseio.com",
+  projectId: "tuckshop-preorder",
+  storageBucket: "tuckshop-preorder.firebasestorage.app",
+  messagingSenderId: "781439361302",
+  appId: "1:781439361302:web:c4934745468ffb9718f037",
+  measurementId: "G-CZL8PFF61P"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
